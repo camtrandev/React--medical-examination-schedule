0402b65882907ef7dac43ce47449829af6f14ebb
camtrandev <camtm2004@gmail.com>
2023-12-12 22:31:59 +0700

    first commit

--- src/store/actions/adminAction.js
+++ src/store/actions/adminAction.js
@@ -1,6 +1,7 @@
 
 import actionTypes from './actionTypes';
-import { getAllCodeService } from '../../services/userService';
+import { getAllCodeService, createNewUserService } from '../../services/userService';
+
 
 
 // export const fetchGenderStart = () => ({
@@ -91,5 +92,27 @@ export const fetchRoleFailed = () => ({
 })
 
 
+export const createNewUser = (data) => {
+    return async (dispatch, getState) => {
+        try {
+            let res = await createNewUserService(data);
+            console.log('check camtrandev:   ', res)
+            if (res && res.errCode === 0) {
+                dispatch(saveUserSuccess(res.data))
+            } else {
+                dispatch(saveUserFailed());
+            }
+        } catch (e) {
+            dispatch(saveUserFailed());
+            console.log('Error', e)
+        }
+    }
+}
 
+export const saveUserSuccess = () => ({
+    type: 'CREATE_USER_SUCCESS'
+})
 
+export const saveUserFailed = () => ({
+    type: 'CREATE_USER_FAILED'
+})
