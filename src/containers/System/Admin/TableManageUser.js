c0af3717dd6235d6fdc0d01d24d72dd6abafb422
camtrandev <camtm2004@gmail.com>
2023-12-28 21:53:58 +0700

    first commit

--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -6,6 +6,23 @@ import './tableManageUser.scss';
 import * as actions from '../../../store/actions'
 
 
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+// import style manually
+import 'react-markdown-editor-lite/lib/index.css';
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+    console.log('handleEditorChange', html, text);
+}
+
+
 class TableManageUser extends Component {
 
     // hàm khởi tạo giá trị đầu tiên khi hàm render nó sẽ chạy vào hàm constructor
@@ -42,46 +59,48 @@ class TableManageUser extends Component {
 
         let arrUsers = this.state.usersRedux
         return (
+            <React.Fragment>
+                <table id='TableManageUser'>
+                    <tbody>
+                        <tr>
+                            <th>Email</th>
+                            <th>FirstName</th>
+                            <th>LastName</th>
+                            <th>Address</th>
+                            <th>Action</th>
+                        </tr>
 
-            <table id='TableManageUser'>
-                <tbody>
-                    <tr>
-                        <th>Email</th>
-                        <th>FirstName</th>
-                        <th>LastName</th>
-                        <th>Address</th>
-                        <th>Action</th>
-                    </tr>
+                        {arrUsers && arrUsers.length > 0 &&
 
-                    {arrUsers && arrUsers.length > 0 &&
+                            arrUsers.map((item, index) => {
+                                return (
+                                    <tr key={index} >
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button
+                                                onClick={() => this.handleEditUser(item)}
+                                                className='btn-edit' >
+                                                <i className="fas fa-pencil-alt"></i>
+                                            </button>
+                                            <button
+                                                onClick={() => this.handleDeleteUser(item)}
+                                                className='btn-delete'>
+                                                <i className="fas fa-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table >
 
-                        arrUsers.map((item, index) => {
-                            return (
-                                <tr key = {index} >
-                                    <td>{item.email}</td>
-                                    <td>{item.firstName}</td>
-                                    <td>{item.lastName}</td>
-                                    <td>{item.address}</td>
-                                    <td>
-                                        <button 
-                                        onClick={() => this.handleEditUser(item)}
-                                        className='btn-edit' >
-                                            <i className="fas fa-pencil-alt"></i>
-                                        </button>
-                                        <button 
-                                        onClick={()=> this.handleDeleteUser(item)}
-                                        className='btn-delete'>
-                                            <i className="fas fa-trash"></i>
-                                        </button>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table >
+                <MdEditor style={{ height: '500px' }} renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} />
 
-
+            </React.Fragment>
         );
     }
 
@@ -97,7 +116,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return {
         fetchUserRedux: () => dispatch(actions.fetchAllUsersStart()),
-        deleteUserRedux: (id)=> dispatch(actions.deleteUser(id))
+        deleteUserRedux: (id) => dispatch(actions.deleteUser(id))
     };
 };
 
